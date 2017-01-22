85adaef7ca2822416fb278f7157cfb02eb2dd60f
Tyler Haas <Tyler.Haas@maritz.com>
2017-01-22 15:04:30 -0600

    include table in view

---
+++ Main.elm
@@ -0,0 +1,22 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+-- Main
+
+
+main : Html msg
+main =
+    view
+
+
+
+-- View
+
+
+view : Html msg
+view =
+    table [ style [ ( "border", "1px solid" ) ] ]
+        [ thead [] [ tr [] [ text "YM Assignments" ] ] ]
